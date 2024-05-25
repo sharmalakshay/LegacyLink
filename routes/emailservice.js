430b6427f606e8cddd0baea9c6043a1dd67539f2
Lakshay Sharma <lakshay0495@gmail.com>
2024-05-25 21:00:11 +0100

    feat: Add domain verification for email service

--- routes/emailservice.js
+++ routes/emailservice.js
@@ -1,11 +1,29 @@
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-if(process.env.EMAIL_DOMAIN_VERIFIED !== true) resend.domains.verify(process.env.EMAIL_DOMAIN).then(() => {
-  console.log('Domain verified');
-}).catch((error) => {
-  console.error('Failed to verify domain: ' + error.message);
-});
+// Get the domains from Resend
+const domains = await resend.domains.list();
+
+// if no domain is found, create a new domain
+if (domains.length === 0) {
+    const domain = await resend.domains.create({
+        name: process.env.EMAIL_DOMAIN,
+    });
+    domains.push(domain);
+}
+
+// Check if the domain is verified
+const email_domain = domains.find(domain => domain.name === process.env.EMAIL_DOMAIN);
+if (!email_domain || email_domain.status !== 'verified') {
+  // If the domain is not verified, verify it
+    const domain_id = email_domain.id;
+    const emailDomainVerification = await resend.domains.verify(domain_id);
+    if (!emailDomainVerification.id) {
+        throw new Error('Failed to verify domain');
+    } else {
+        console.log('Domain verified');
+    }
+}
 
 const email_from = process.env.EMAIL_FROM;
 
