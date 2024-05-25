c6b3d519e031ced1508423a8edfadddf62960861
Lakshay Sharma <lakshay0495@gmail.com>
2024-05-25 21:32:42 +0100

    fix the email service setup to use async/await

--- routes/emailservice.js
+++ routes/emailservice.js
@@ -26,22 +26,28 @@ async function setupEmailService() {
         }
     }
 }
-setupEmailService().catch(console.error);
+(async () => {
+    try {
+        await setupEmailService();
+    } catch (error) {
+        console.error(error);
+    }
+})();
 
 const email_from = process.env.EMAIL_FROM;
 
 async function sendEmail(to, subject, html) {
-  try {
-    const data = await resend.emails.send({
-        from: email_from,
-        to: to,
-        subject: subject,
-        html: html,
-    });
-    return data;
-  } catch (error) {
-    throw new Error('Failed to send email: ' + error.message);
-  }
+    try {
+        const data = await resend.emails.send({
+            from: email_from,
+            to: to,
+            subject: subject,
+            html: html,
+        });
+        return data;
+    } catch (error) {
+        throw new Error('Failed to send email: ' + error.message);
+    }
 }
 
 module.exports = { sendEmail };
