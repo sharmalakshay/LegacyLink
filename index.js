efad4f5f436afd4825639b94adec1c0ab967b19f
Lakshay Sharma <lakshay0495@gmail.com>
2024-06-08 17:16:41 +0100

    feat: Add Bootstrap CSS and JS files for styling and interactivity

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ mongoose.connect(mongoURL, {
 
 // Specify the public folder for serving static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css/', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
+app.use('/js/', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
 
 // Middleware for session management
 app.use(session({
