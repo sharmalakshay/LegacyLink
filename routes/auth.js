0d207a8f10037f0d2fcae8beb6c7383e8ecc8000
Lakshay Sharma <lakshay0495@gmail.com>
2024-10-21 13:17:43 +0100

    a redirect has to be set to call the redirect_message endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -126,7 +126,7 @@ router.post('/reset_password', async (req, res) => {
         if (isMatch) {
             const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
             res.cookie('token', token, { httpOnly: true }); // set cookie
-            return res.render('redirect_message', { message: 'New password is same as old password. Logging in!'});
+            return res.render('redirect_message', { message: 'New password is same as old password. Logging in!', redirect: '' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -134,7 +134,7 @@ router.post('/reset_password', async (req, res) => {
         await user.save();
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.cookie('token', token, { httpOnly: true }); // set cookie
-        return res.render('redirect_message', { message: 'Password reset successfully. Logging in!' });
+        return res.render('redirect_message', { message: 'Password reset successfully. Logging in!', redirect: '' });
     } catch (error) {
         console.error(error.message);
         res.status(500).json({ message: 'Server Error.' });
